a6a646b206b7a86b48d03bd1faf6d439b5db1846
tangqi <tangqihlw@bbktel.com>
2017-09-29 15:15:17 +0800

    Merge branch 'git-branch1' into git-branch2
    # Conflicts:
    #	src/main/java/com/xdd/mdemo/pack/blog/MyBatisRunner.java

--- src/main/java/com/xdd/mdemo/pack/blog/MyBatisRunner.java
+++ src/main/java/com/xdd/mdemo/pack/blog/MyBatisRunner.java
@@ -35,6 +35,7 @@ public class MyBatisRunner {
             if(sta!=null){
                 System.out.println(sta.getStoreName());
             }
+            System.out.println("222222222222");
         } finally {
             session.close();
         }
